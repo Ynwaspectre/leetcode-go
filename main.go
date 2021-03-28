89f39bdc5c05490c3210d30943511a16e875b03c
ynwaspectre <ynwaspectre@gmail.com>
2021-03-28 23:29:22 +0800

    两数相加 调试中

--- main.go
+++ main.go
@@ -24,23 +24,66 @@ func printNode(node *ListNode) {
 	}
 }
 
-func deleteDuplicates(head *ListNode) *ListNode {
-	head1 := head
-	for head1 != nil && head1.Next != nil {
-		if head1.Next.Val == head1.Val {
-			head1.Next = head1.Next.Next
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	l3 := &ListNode{0, nil}
+	l4 := l3
+	jinwei := 0
+	for l1 != nil && l2 != nil {
+		value := l1.Val + l2.Val + jinwei
+		jinwei = 0
+		if value >= 10 {
+			jinwei++ //下一步是否需要进位
+			l3.Val = value % 10
 		} else {
-			head1 = head1.Next
+			l3.Val = value
+		}
+		l1 = l1.Next
+		l2 = l2.Next
+		l3.Next = &ListNode{0, nil}
+		l3 = l3.Next
+	}
+	if l1 == nil && l2 != nil {
+		for l2 != nil {
+			value := l2.Val + jinwei
+			if value > 10 {
+				jinwei++ //下一步是否需要进位
+				l3.Val = value % 10
+			} else {
+				l3.Val = value
+			}
+			l2 = l2.Next
+			l3 = l3.Next
+			jinwei = 0
+		}
+	} else if l2 == nil && l1 != nil {
+		for l1 != nil {
+			value := l1.Val + jinwei
+			if value > 10 {
+				jinwei++ //下一步是否需要进位
+				l3.Val = value % 10
+			} else {
+				l3.Val = value
+			}
+			l1 = l1.Next
+			l3 = l3.Next
+			jinwei = 0
+		}
+	} else {
+		if jinwei == 1 {
+			l3 = &ListNode{1, nil}
+		} else {
+			l3 = nil
 		}
 	}
-	return head
+	return l4
 }
 
 func main() {
-	nums := []int{1, 2, 2, 2, 2, 2, 2, 3, 4, 5, 5}
-	node := createNode(nums)
+	//l1 := createNode([]int{2, 4, 3})
+	//l2 := createNode([]int{5, 6, 4})
+	l1 := createNode([]int{9, 9, 9, 9, 9, 9, 9})
+	l2 := createNode([]int{9, 9, 9, 9})
+	node := addTwoNumbers(l1, l2)
 	printNode(node)
-	fmt.Println("---")
 
-	printNode(deleteDuplicates(node))
 }
