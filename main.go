d4681389ded24c16710a57632587d092a19e7a67
ynwaspectre <ynwaspectre@gmail.com>
2021-03-28 22:25:51 +0800

    删除排序链表中的重复元素

--- main.go
+++ main.go
@@ -1,7 +1,46 @@
 package main
 
-import ()
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func createNode(nums []int) *ListNode {
+	head := &ListNode{nums[0], nil}
+	head1 := head
+	for i := 1; i < len(nums); i++ {
+		head1.Next = &ListNode{nums[i], nil}
+		head1 = head1.Next
+	}
+	return head
+}
+
+func printNode(node *ListNode) {
+	for node != nil {
+		fmt.Print(node.Val)
+		node = node.Next
+	}
+}
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	head1 := head
+	for head1 != nil && head1.Next != nil {
+		if head1.Next.Val == head1.Val {
+			head1.Next = head1.Next.Next
+		} else {
+			head1 = head1.Next
+		}
+	}
+	return head
+}
 
 func main() {
+	nums := []int{1, 2, 2, 2, 2, 2, 2, 3, 4, 5, 5}
+	node := createNode(nums)
+	printNode(node)
+	fmt.Println("---")
 
+	printNode(deleteDuplicates(node))
 }
--- 链表/删除排序链表中的重复元素.go
+++ 链表/删除排序链表中的重复元素.go
@@ -36,6 +36,17 @@ type ListNode struct {
 	Next *ListNode
 }
 
+//
+// 虽然是简单 但是对链表并不是很熟悉，不过还是自己做出来了，本来想用个map来保存遇到的 然后遍历的时候map
+//里有就删除 后来看是有序的应该可以连续的
 func deleteDuplicates(head *ListNode) *ListNode {
-
+	head1 := head //先找个替代的去遍历
+	for head1 != nil && head1.Next != nil {
+		if head1.Next.Val == head1.Val {
+			head1.Next = head1.Next.Next
+		} else { //刚开始这边没加else 没通过 只会对1个重复的进行删除 后面加了else后 有重复的不会向前走，就会一直删除重复的 比较关键
+			head1 = head1.Next
+		}
+	}
+	return head
 }
