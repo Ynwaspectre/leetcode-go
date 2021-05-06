ecc4742b5ad55c93406ad979737826c6e7c16ace
ynwaspectre <ynwaspectre@gmail.com>
2021-05-06 11:13:16 +0800

    设计停车系统

--- 数组/设计停车系统.go
+++ 数组/设计停车系统.go
@@ -48,7 +48,25 @@ func Constructor(big int, medium int, small int) ParkingSystem {
 }
 
 func (this *ParkingSystem) AddCar(carType int) bool {
-
+	if carType == 1 {
+		if this.Big > 0 {
+			this.Big--
+			return true
+		}
+		return false
+	} else if carType == 2 {
+		if this.Medium > 0 {
+			this.Medium--
+			return true
+		}
+		return false
+	} else {
+		if this.Small > 0 {
+			this.Small--
+			return true
+		}
+		return false
+	}
 }
 
 /**
